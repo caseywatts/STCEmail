2edb87b0b8f76601caf4e2891bbce7f3607369a2
caseywatts <casey.s.watts@gmail.com>
2014-07-10 21:15:08 -0400

    use session[:cas_user] instead of params[:netid]

--- STCEmailer.rb
+++ STCEmailer.rb
@@ -12,10 +12,10 @@ require 'sinatra/reloader' if development?
   end
 
   def create_incident(params)
-    #requires params[:netid] to work, others are optional
     ServiceNow::Configuration.configure(:sn_url => 'https://yaletest.service-now.com', :sn_username => ENV['SN_USERNAME'], :sn_password => ENV['SN_PASSWORD'])
     inc = ServiceNow::Incident.new
-    inc.caller_id = ServiceNow::User.find(params[:netid]).sys_id
+    # the `params[:netid]` isn't as secure as the `session[:cas_user]` that comes from rubycas
+    inc.caller_id = ServiceNow::User.find(session[:cas_user]).sys_id
     inc.short_description = "Report From STCEmailApp"
     inc.description = %{
       Name: #{params[:first]} #{params[:last]}
@@ -52,7 +52,7 @@ require 'sinatra/reloader' if development?
     # Hopefully after the incident is created some data is returned and we can display that?
     # @inc = create_incident(params)
     @inc = 'an incident'
-    @netid = params[:netid]
+    @netid = session[:cas_user]
     # erb :confirmation
     params.inspect
   end
